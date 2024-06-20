ee6a9e99af2961e6682a2bc0977cd79d394a97b5
Jahle <Jahle@ABLACKMAN>
2024-06-20 11:05:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Assignment_7._2._3
+{
+    class Program
+    {
+        static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (char c in s)
+                charCount[c] = charCount.GetValueOrDefault(c) + 1;
+
+            foreach (char c in t)
+            {
+                if (!charCount.ContainsKey(c) || charCount[c] == 0)
+                    return false;
+                charCount[c]--;
+            }
+
+            return true;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(IsAnagram("anagram", "nagaram")); // Output: True
+            Console.WriteLine(IsAnagram("rat", "car")); // Output: False
+        }      
+    }
+}
